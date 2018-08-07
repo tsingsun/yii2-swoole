2e57da81f0ce4b818682486e90cd7a14aa8e5ae5
tsingsun <tsingsun@live.cn>
2018-08-07 10:06:39 +0800

    提供Yii访问swooleServer的能力

--- demo/swoole_http_server.php
+++ demo/swoole_http_server.php
@@ -1,13 +1,14 @@
 <?php
 
 use \tsingsun\swoole\server\Server;
+
 /**
  * Created by PhpStorm.
  * User: tsingsun
  * Date: 2017/2/28
  * Time: 上午11:15
  */
-defined('WEBROOT') or define('WEBROOT', __DIR__.'/yii2-app-basic/web');
+defined('WEBROOT') or define('WEBROOT', __DIR__ . '/yii2-app-basic/web');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
@@ -15,22 +16,23 @@ defined('COROUTINE_ENV') or define('COROUTINE_ENV', true);
 
 require(__DIR__ . '/yii2-app-basic/vendor/autoload.php');
 $config = [
-    'class'=>'tsingsun\swoole\server\HttpServer',
+    'class' => 'tsingsun\swoole\server\HttpServer',
 //    'timeout'=>2,
     'setting' => [
-        'daemonize'=>0,
-        'max_coro_num'=>3000,
-//        'reactor_num'=>1,
-        'worker_num'=>1,
+        'daemonize' => 0,
+        'max_coro_num' => 3000,
+        'reactor_num' => 1,
+        'worker_num' => 1,
+        'task_worker_num' => 2,
         'pid_file' => __DIR__ . '/yii2-app-basic/runtime/testHttp.pid',
-        'log_file' => __DIR__.'/yii2-app-basic/runtime/logs/swoole.log',
-        'debug_mode'=> 0,
+        'log_file' => __DIR__ . '/yii2-app-basic/runtime/logs/swoole.log',
+        'debug_mode' => 0,
         'enable_coroutine' => COROUTINE_ENV
 
     ],
 ];
 
-Server::run($config,function (Server $server){
+Server::run($config, function (Server $server) {
     $starter = new \tsingsun\swoole\bootstrap\WebApp($server);
     //初始化函数独立,为了在启动时,不会加载Yii相关的文件,在库更新时采用reload平滑启动服务器
     $starter->init = function (\tsingsun\swoole\bootstrap\BaseBootstrap $bootstrap) {
@@ -44,8 +46,20 @@ Server::run($config,function (Server $server){
         );
         Yii::setAlias('@webroot', WEBROOT);
         Yii::setAlias('@web', '/');
+        //如果需要原生的swoole服务，可以这样
+        Yii::$swooleServer = $bootstrap->getServer()->getSwoole();
         $bootstrap->appConfig = $config;
+
     };
+    $server->getSwoole()->on("Task", function (swoole_server $serv, $task_id, $from_id, $data) {
+        echo "Tasker进程接收到数据";
+        echo "#{$serv->worker_id}\tonTask: [PID={$serv->worker_pid}]: task_id=$task_id, data_len=" . strlen($data) . "." . PHP_EOL;
+        $serv->finish($data);
+    }
+    );
+    $server->getSwoole()->on("Finish", function (swoole_server $serv, $task_id, $data) {
+        echo "Task#$task_id finished, data_len=" . strlen($data) . PHP_EOL;
+    });
     $server->bootstrap = $starter;
     $server->start();
 });
